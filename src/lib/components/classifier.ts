7acf8321eedfdb51373e6ddfb76fa6a74498021a
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2025-06-23 18:37:10 +0200

    Added Custom Tflite Model Support (#2)
    * loading module working after applying a patch
    
    * Update classifier.ts
    
    * Create classification.tflite
    
    * Update README.md
    
    * Update README.md

--- src/lib/components/classifier.ts
+++ src/lib/components/classifier.ts
@@ -1,56 +1,32 @@
-import * as tf from '@tensorflow/tfjs';
-import * as mobilenet from '@tensorflow-models/mobilenet';
+// Import @tensorflow-models/tasks.
+import * as tfTask from '@tensorflow-models/tasks';
+import type { ODCustomModelTFLite } from '@tensorflow-models/tasks/dist/tasks/object_detection/custom_model_tflite';
 
-let fileuploadprops = {
-	id: 'inputImage'
-};
+let model: tfTask.ICCustomModelTFLite | null = null;
+//let model: ODCustomModelTFLite | null = null;
 
-let predictionsResult: Prediction[] = [];
+export async function loadClassifier() {
+	model = await tfTask.ImageClassification.CustomModel.TFLite.load({
+		model: 'model/classification.tflite'
+	});
 
-let files: FileList;
-
-let model: mobilenet.MobileNet;
-let predictionsLoading = false;
-
-interface Prediction {
-	className: string;
-	probability: number;
+	/*
+	model1 = await tfTask.ObjectDetection.CustomModel.TFLite.load({
+		model: 'model/model.tflite'
+	});
+	*/
 }
 
-const loadModel = async () => {
-	const version = 2;
-	const alpha = 0.5;
-	model = await mobilenet.load({ version, alpha });
-	console.log('Model loaded successfully');
-	return model;
-};
+export async function classifyImage(image: HTMLImageElement) {
+	if (!model) {
+		await loadClassifier();
+	}
 
-const preprocessImage = (imageElement: HTMLImageElement) => {
-	const imageTensor = tf.browser.fromPixels(imageElement);
-	const resizedImageTensor = tf.image.resizeBilinear(imageTensor, [224, 224]);
-	// const normalizedImageTensor = resizedImageTensor.div(255.0);
-	return resizedImageTensor;
-};
+	if (!model) {
+		throw new Error('Model not loaded');
+	}
+	const result = await model.predict(image);
+	console.log(result.classes);
 
-const classifyImage = async (
-	model: mobilenet.MobileNet,
-	preprocessedImage: tf.Tensor3D
-) => {
-	const predictions = await model.classify(preprocessedImage);
-	return predictions;
-};
-
-const predictionImage = async (imgDisplay: HTMLImageElement) => {
-	predictionsLoading = true;
-	const model = await loadModel();
-	const preprocessedImage = preprocessImage(imgDisplay);
-	predictionsResult = await classifyImage(model, preprocessedImage);
-	predictionsLoading = false;
-	return predictionsResult;
-};
-
-export async function classify(
-	imageElement: HTMLImageElement
-): Promise<Prediction[]> {
-	return await predictionImage(imageElement);
+	return result;
 }
