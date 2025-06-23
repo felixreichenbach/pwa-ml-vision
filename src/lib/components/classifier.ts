d72d409e5e06a35901eb978238b5d4e611bfa75a
felixreichenbach <felix.reichenbach@viam.com>
2025-06-23 18:16:58 +0200

    Update classifier.ts

--- src/lib/components/classifier.ts
+++ src/lib/components/classifier.ts
@@ -1,12 +1,20 @@
 // Import @tensorflow-models/tasks.
 import * as tfTask from '@tensorflow-models/tasks';
+import type { ODCustomModelTFLite } from '@tensorflow-models/tasks/dist/tasks/object_detection/custom_model_tflite';
 
 let model: tfTask.ICCustomModelTFLite | null = null;
+//let model: ODCustomModelTFLite | null = null;
 
 export async function loadClassifier() {
 	model = await tfTask.ImageClassification.CustomModel.TFLite.load({
+		model: 'model/classification.tflite'
+	});
+
+	/*
+	model1 = await tfTask.ObjectDetection.CustomModel.TFLite.load({
 		model: 'model/model.tflite'
 	});
+	*/
 }
 
 export async function classifyImage(image: HTMLImageElement) {
